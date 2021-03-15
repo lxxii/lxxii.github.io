6b7ab399619f6ef4cac307a8ad7bae0bfc37f6f3
lxxii <30430118+lxxii@users.noreply.github.com>
2021-03-15 15:35:11 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@ session_start();
 // Enter the $site_owners_email that you want to emails to be sent to.
 // Example $site_owners_email = "joe.doe@yourdomain.com";
 
-$site_owners_email = "joe.doe@yourdomain.com";  // Replace this with your own email address
+$site_owners_email = "zozo8@qq.com";  // Replace this with your own email address
 $subject = "Contact Form";                       // subject to email             
 
 
